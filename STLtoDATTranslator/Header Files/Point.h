2ab2bdb172c6cec8e82e1facf12c2e14b525ec77
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-10-08 12:35:04 +0530

    Update Point.h

--- STLtoDATTranslator/Header Files/Point.h
+++ STLtoDATTranslator/Header Files/Point.h
@@ -1,10 +1,8 @@
 #pragma once
 
-using namespace std;
-
 class Point {
-    public:
-        int x, y, z;
-        Point(int x, int y, int z) : x(x), y(y), z(z) {}
-        ~Point() {}
+public:
+	int x, y, z;
+	Point(int x=0,int y=0,int z=0);
+	~Point();
 };
