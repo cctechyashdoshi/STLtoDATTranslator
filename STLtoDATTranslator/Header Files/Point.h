d2ddc0c737cd909cc1b55801db8fbc4b2fad1832
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-10-10 19:39:29 +0530

    Delete STLtoDATTranslator directory

--- STLtoDATTranslator/Header Files/Point.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Point {
-public:
-	int x, y, z;
-	Point(int x=0,int y=0,int z=0);
-	~Point();
-};
--- STLtoDATTranslator/Header Files/Triangle.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Point.h"
-
-class Triangle {
-public:
-    Point p1, p2, p3;
-    Triangle(Point p1, Point p2, Point p3);
-    ~Triangle();
-};
