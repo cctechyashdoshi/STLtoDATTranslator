e13c3b7f6428e85115efe7158e4e4e4166149d23
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-10-10 19:50:31 +0530

    Revert "Stl to dat yash doshi 1"

---
+++ STLtoDATTranslator/Source Files/FileRead.cpp
@@ -0,0 +1,32 @@
+#include "FileRead.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+Read::Read()
+{
+}
+
+void Read::read() {
+    ifstream myFile("cube.stl");
+
+    if (myFile.is_open()) {
+        while (getline(myFile, line)) {
+            if (line.find("vertex") != string::npos) {
+                int pos = line.find("vertex");
+                data += line.substr(pos + 7);
+                data += ' ';
+            }
+        }
+        myFile.close();
+    }
+    else {
+        cout << "Unable to open file" << endl;
+    }
+}
+
+Read::~Read()
+{
+}
---
+++ STLtoDATTranslator/Source Files/Triangulation.cpp
@@ -0,0 +1,37 @@
+#include "Triangulation.h"
+
+void Triangulation::processString(const string& data)
+{
+    istringstream iss(data);
+    double value;
+    int currentIndex = 0;
+
+    while (iss >> value) {
+        if (vertexIndexMap.find(value) == vertexIndexMap.end()) {
+            vertexIndexMap[value] = currentIndex;
+            uniqueVertices.push_back(value);
+            currentIndex++;
+        }
+    }
+}
+
+vector<Triangle> Triangulation::createTriangles(const string& data)
+{
+    vector<Triangle> triangles;
+    istringstream iss(data);
+    vector<int> indices;
+
+    double val;
+    while (iss >> val) {
+        indices.push_back(vertexIndexMap[val]);
+
+        if (indices.size() == 9) {
+            Point p1(indices[0], indices[1], indices[2]);
+            Point p2(indices[3], indices[4], indices[5]);
+            Point p3(indices[6], indices[7], indices[8]);
+            triangles.emplace_back(p1, p2, p3);
+            indices.clear();
+        }
+    }
+    return triangles;
+}
