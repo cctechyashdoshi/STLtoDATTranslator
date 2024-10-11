3328f0e53f22dab715e7cbfd676c3f5c87c0fa1f
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-10-11 12:36:44 +0530

    Update Triangle.cpp

--- STLtoDATTranslator/Source Files/Triangle.cpp
+++ STLtoDATTranslator/Source Files/Triangle.cpp
@@ -7,6 +7,19 @@ Triangle::Triangle(Point p1, Point p2, Point p3)
 	this->p3 = p3;
 }
 
+Point Triangle::getPointP1()
+{
+	return p1;
+}
+Point Triangle::getPointP2()
+{
+	return p2;
+}
+Point Triangle::getPointP3()
+{
+	return p3;
+}
+
 Triangle::~Triangle()
 {
 }
