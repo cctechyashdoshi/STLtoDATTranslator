6897b380644c230e27735cd2715f27030137e470
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-10-13 11:15:57 +0530

    Update FileWrite.h

--- STLtoDATTranslator/Header Files/FileWrite.h
+++ STLtoDATTranslator/Header Files/FileWrite.h
@@ -4,8 +4,6 @@
 #include<triangle.h>
 #include<iomanip>
 
-using namespace std;
-
 class Write {
 public:
     void writeFile(const std::string& filename, const std::vector<Triangle>& triangles, const std::vector<double>& uniqueVertices);
