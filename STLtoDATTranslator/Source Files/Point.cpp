94ebe02e1abe04b1efdd57bed9fe5f1fe9993aa4
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-10-13 11:57:13 +0530

    Remove "this" keyword to make it more readable

--- STLtoDATTranslator/Source Files/Point.cpp
+++ STLtoDATTranslator/Source Files/Point.cpp
@@ -2,12 +2,9 @@
 
 
 
-Point::Point(int x, int y, int z)
-{
-	this->x = x;
-	this->y = y;
-	this->z = z;
-}
+Point::Point(int x, int y, int z) 
+    : x(x), y(y), z(z) 
+{};
 
 int Point::getX(){return x;};
 int Point::getY(){return y;};
