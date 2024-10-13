b0dd647bd668f3a724820f9f1329a279d167a37e
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-10-13 11:17:54 +0530

    Increase readability of code

--- STLtoDATTranslator/Source Files/Point.cpp
+++ STLtoDATTranslator/Source Files/Point.cpp
@@ -9,22 +9,8 @@ Point::Point(int x, int y, int z)
 	this->z = z;
 }
 
-int Point::getX()
-{
-	return x;
-}
-int Point::getY()
-{
-	return y;
-}
-int Point::getZ()
-{
-	return z;
-}
+int Point::getX(){return x;};
+int Point::getY(){return y;};
+int Point::getZ(){return z;};
 
-Point::~Point()
-{
-}
-
-
-
+Point::~Point(){};
