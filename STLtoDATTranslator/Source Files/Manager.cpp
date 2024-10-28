87f15cce79e73b70c303f828dfaf7d27dd3a757f
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-10-28 15:17:44 +0530

    Bug Fix

---
+++ STLtoDATTranslator/Source Files/Manager.cpp
@@ -0,0 +1,15 @@
+#include "../Header Files/Manager.h"
+#include "../Header Files/FileRead.h"
+#include "../Header Files/FileWrite.h"
+#include "../Header Files/Triangulation.h"
+
+void Manager::managerMain() 
+{
+    STLReader reader;
+    FileWrite writer;
+    Triangulation triangulation;
+
+    std::string inputFile = "../Test Cases/cube.stl";
+    reader.read(inputFile, triangulation);
+    writer.writeToFile("output.dat", triangulation);
+}
